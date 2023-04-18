69468d293599ea1de8f99a935b0ae0961f62f4e2
unknown <JomariDalugdug>
2023-04-18 09:56:14 +0800

    update: auth

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -56,7 +56,7 @@ class AuthController extends Controller
 
             $validator = Validator::make($request->all(), [
                 'email' => 'required|email',
-                'password' => 'required|string|min:8'
+                'password' => 'required|string'
             ]);
 
             if ($validator->fails()) {
