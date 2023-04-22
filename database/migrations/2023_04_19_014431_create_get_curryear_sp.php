49934f4007b7286fa28924055b8311f6d827b15d
unknown <menard.alcantara@simplevia.com>
2023-04-22 15:27:44 +0800

    changes in storeproc

--- database/migrations/2023_04_19_014431_create_get_curryear_sp.php
+++ database/migrations/2023_04_19_014431_create_get_curryear_sp.php
@@ -15,7 +15,7 @@ return new class extends Migration
         $procedure = " DROP PROCEDURE IF EXISTS `get_CurrYear`;
         CREATE PROCEDURE `get_CurrYear`()
         BEGIN
-            select id, account_title, account_code, date_effectivity, status
+            select *
             from coa_assets
             where date_effectivity = ( select max(date_effectivity) from coa_assets );
         END
