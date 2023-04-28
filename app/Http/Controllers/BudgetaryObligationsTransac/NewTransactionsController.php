69a1c1c554301faa59db79aca1ffb543f2249cd1
shinobeez <deneijabal@gmail.com>
2023-04-28 19:45:04 +0800

    add some changes

--- app/Http/Controllers/BudgetaryObligationsTransac/NewTransactionsController.php
+++ app/Http/Controllers/BudgetaryObligationsTransac/NewTransactionsController.php
@@ -3,9 +3,50 @@
 namespace App\Http\Controllers\BudgetaryObligationsTransac;
 
 use App\Http\Controllers\Controller;
+use App\Models\DMBudgetaryObligationsTransac;
 use Illuminate\Http\Request;
 
 class NewTransactionsController extends Controller
 {
-    //
+
+    public function insertNewTransac(Request $request){
+
+try{
+
+    $request->validate([
+
+
+
+
+
+     ]);
+
+     
+    $insertNew = DMBudgetaryObligationsTransac::create([
+
+    'docu_type_id'=>$request->docu_type_id,
+    'transaction_id'=>$request->transaction_id,
+    'department'=>$request->department,
+    'processing_slip_number'=>$request->processing_slip_number,
+    'description'=>$request->description,
+    'amount'=>$request->amount,
+    'current_bearer'=>$request->current_bearer,
+    'current_bearer_dept'=>$request->current_bearer_dept,
+    'current_bearer_contact_number'=>$request->current_bearer_contact_number,
+    'current_bearer_email'=>$request->current_bearer_email,
+    'status'=>$request->status
+
+
+    ]);
+
+}catch (\Throwable $th){
+    return response()->json([
+        'status' => false,
+        'message' => 'Something went wrong',
+        'error' => $th->getMessage()
+    ]);
 }
+    
+}
+}
+
