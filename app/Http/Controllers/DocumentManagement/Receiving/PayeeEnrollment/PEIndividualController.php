505e152ae7c98dfd8e4be322e1cb13c1a74d8c30
shinobeez <deneijabal@gmail.com>
2023-05-05 16:39:07 +0800

    payee enrollment added

---
+++ app/Http/Controllers/DocumentManagement/Receiving/PayeeEnrollment/PEIndividualController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers\DocumentManagement\Receiving\PayeeEnrollment;
+
+use App\Http\Controllers\Controller;
+use App\Models\PEIndividual;
+use Illuminate\Http\Request;
+
+class PEIndividualController extends Controller
+{
+
+    public function storeIndividual(Request $request){
+        try{
+        $individualData = PEIndividual::create($this->validateIndividualData());
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Successfully Enrolled',
+            'data' => $individualData, 
+        ]);
+
+        
+    }catch (\Throwable $th){
+        return response()->json([
+            'status' => false,
+            'message' => 'Something went Wrong',
+            'error' => $th->getMessage()
+        ]);
+    }
+}
+
+
+    public function validateIndividualData()
+    {
+         return request()->validate([
+
+            'type_of_payee_id' => 'required',
+            'client_type' => 'required',
+            'last_name' => 'required',
+            'first_name' => 'required',
+            'middle_name' => 'required',
+            'suffix' => 'required',
+            'building_no' => 'required',
+            'lot_no' => 'required',
+            'street' => 'required',
+            'barangay' => 'required',
+            'municipality_city' => 'required',
+            'province' => 'required',
+            'country' => 'required',
+            'zip_code' => 'required',
+            'date_registered' => 'required',
+            'status' => 'required'
+        ]);
+    }
+
+    
+}
+    
