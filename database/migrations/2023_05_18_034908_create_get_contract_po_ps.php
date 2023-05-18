d9ea0ca7c40e5b8e1db54a8b614ec5fe0a5a7251
unknown <menard.alcantara@simplevia.com>
2023-05-18 14:16:57 +0800

    contracts po previous

--- database/migrations/2023_05_18_034908_create_get_contract_po_ps.php
+++ database/migrations/2023_05_18_034908_create_get_contract_po_ps.php
@@ -17,11 +17,15 @@ return new class extends Migration
         BEGIN
 
         select dm_contractpos.id, DATE_FORMAT(dm_contractpos.created_at, '%M %d %Y ') AS date, DATE_FORMAT(dm_contractpos.created_at, '%h:%i:%s') AS time, 
-        transaction_id, departments.department_name, payee_name, description, amount
+        transaction_id, contract_dept.department_name, payee_name, description, amount, current_bearer, current_bearer_dept.department_name AS current_bearer_dept_name,
+
+		current_bearer_contact_number, current_bearer_contact_number
             
         from dm_contractpos
-        join departments
-        where departments.department_code = dm_contractpos.department_id;
+        join 
+		departments AS contract_dept ON contract_dept.department_code = dm_contractpos.department_id
+		JOIN
+		departments AS current_bearer_dept ON current_bearer_dept.department_code = dm_contractpos.current_bearer_dept;
         END";
 
         DB::unprepared($procedure);
