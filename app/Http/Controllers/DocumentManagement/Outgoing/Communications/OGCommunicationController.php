0413d4deb7e7f0c5f404d93532797c85768e318b
unknown <menard.alcantara@simplevia.com>
2023-05-30 15:52:02 +0800

    updated outgoing with repositories

---
+++ app/Http/Controllers/DocumentManagement/Outgoing/Communications/OGCommunicationController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\DocumentManagement\Outgoing\Communications;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Models\CreateCommunication;
+use App\Http\Controllers\Controller;;
+use App\Repositories\DocumentManagement\Outgoing\Communications\OutgoingCommRepository;
+
+class OGCommunicationController extends Controller
+{
+
+    private $outgoingCommRepo;
+
+    public function __construct(OutgoingCommRepository $outgoingCommRepo)
+    {
+        $this->outgoingCommRepo = $outgoingCommRepo;
+    }
+
+    public function list()
+    {
+        return response()->json(['list' => DB::select('CALL list_comm_outgoing()')]);
+    }
+
+    public function listMc()
+    {
+        return response()->json(['listMc' => CreateCommunication::all('transac_id')]);
+    }
+
+    public function searchMc(Request $request)
+    {
+        return $this->outgoingCommRepo->searchMc($request);
+    }
+
+    public function update(Request $request)
+    {
+        return $this->outgoingCommRepo->update($request);
+    }
+
+    public function transmital(Request $request)
+    {
+        return $this->outgoingCommRepo->transmital($request);
+    }
+
+    public function updateOutgoing(Request $request)
+    {
+        return $this->outgoingCommRepo->updateOutgoing($request);
+    }
+}
--- app/Repositories/DocumentManagement/Outgoing/Communications/OutgoingCommRepository.php
+++ app/Repositories/DocumentManagement/Outgoing/Communications/OutgoingCommRepository.php
@@ -1,30 +1,19 @@
 <?php
 
-namespace App\Http\Controllers\DocumentManagement\Outgoing\Communication;
+namespace App\Repositories\DocumentManagement\Outgoing\Communications;
 
 use Carbon\Carbon;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 use App\Models\CreateCommunication;
-use App\Http\Controllers\Controller;
 use App\Models\ReceiveCommunications;
 use App\Models\DmComTransmitalCounter;
 
-class OGCommunicationController extends Controller
+class OutgoingCommRepository
 {
-    public function list(){
-        return response()->json(['list' => DB::select('CALL list_comm_outgoing()')]);
-    }
-
-    public function listMc(){
-        return response()->json(['listMc' => CreateCommunication::all('transac_id')]);
-    }
-
-    public function searchMc(Request $request){
-        try{
-
+    public function searchMc($request)
+    {
+        try {
             return response()->json(['data' => CreateCommunication::where('transac_id', $request->mc_no)->first()]);
-        }catch(\Throwable $th){
+        } catch (\Throwable $th) {
             return response()->json([
                 'status' => true,
                 'message' => 'Something went wrong!',
@@ -33,15 +22,15 @@ class OGCommunicationController extends Controller
         }
     }
 
-    public function update(Request $request){
-        try{
+    public function update($request)
+    {
+        try {
 
             $update = CreateCommunication::where('transac_id', $request->mc_no);
             $update->update($request->all());
 
             return response()->json(['message' => 'Your entry has been successfully saved.']);
-
-        }catch(\Throwable $th){
+        } catch (\Throwable $th) {
             return response()->json([
                 'status' => true,
                 'message' => 'Something went wrong!',
@@ -50,15 +39,16 @@ class OGCommunicationController extends Controller
         }
     }
 
-    public function transmital(Request $request){
-        try{
+    public function transmital($request)
+    {
+        try {
 
             $date = Carbon::now()->format('Y');
 
             $table = DmComTransmitalCounter::all();
-            if($table->isEmpty()){
+            if ($table->isEmpty()) {
                 $transmital = "COM-" . $date . "-" . str_pad(1, 5, '0', STR_PAD_LEFT);
-            }else{
+            } else {
                 $lastId = DmComTransmitalCounter::latest('id')->select('id')->first();
                 $botId = $lastId['id'];
                 $transmital = "COM-" . $date . "-" . str_pad(++$botId, 5, '0', STR_PAD_LEFT);
@@ -66,19 +56,18 @@ class OGCommunicationController extends Controller
 
             DmComTransmitalCounter::create(['com_transmital' => $transmital]);
 
-            for($i=0; $i<count($request->transac_id); $i++){  
-                    if(substr($request->transac_id[$i], 0, 3) == "COM"){
-                        $addTransmital = ReceiveCommunications::where('transaction_id_num', $request->transac_id[$i]);
-                        $addTransmital->update(['og_transmital_no' => $transmital]);
-                    }else{
-                        $addTransmital = CreateCommunication::where('transac_id', $request->transac_id[$i]);
-                        $addTransmital->update(['og_transmital_no' => $transmital]);
-                    }
+            for ($i = 0; $i < count($request->transac_id); $i++) {
+                if (substr($request->transac_id[$i], 0, 3) == "COM") {
+                    $addTransmital = ReceiveCommunications::where('transaction_id_num', $request->transac_id[$i]);
+                    $addTransmital->update(['og_transmital_no' => $transmital]);
+                } else {
+                    $addTransmital = CreateCommunication::where('transac_id', $request->transac_id[$i]);
+                    $addTransmital->update(['og_transmital_no' => $transmital]);
+                }
             }
 
-            return response()->json(['message' => 'Your entry has been successfully saved under Transmittal ID Number '.$transmital]);
-
-        }catch(\Throwable $th){
+            return response()->json(['message' => 'Your entry has been successfully saved under Transmittal ID Number ' . $transmital]);
+        } catch (\Throwable $th) {
             return response()->json([
                 'status' => true,
                 'message' => 'Something went wrong!',
@@ -87,27 +76,26 @@ class OGCommunicationController extends Controller
         }
     }
 
-    public function updateOutgoing(Request $request){
-        try{
+    public function updateOutgoing($request)
+    {
+        try {
 
-            for($i=0; $i<count($request->transac_id); $i++){  
-                echo $request->transac_id[$i].'<br>';
-                if(substr($request->transac_id[$i], 0, 3) == "COM"){
+            for ($i = 0; $i < count($request->transac_id); $i++) {
+                if (substr($request->transac_id[$i], 0, 3) == "COM") {
                     $addTransmital = ReceiveCommunications::where('transaction_id_num', $request->transac_id[$i]);
                     $addTransmital->update([
                         'og_sender' => $request->og_sender,
                         'og_received_by' => $request->og_received_by,
                         'og_date' => $request->og_date
                     ]);
-                }else{
+                } else {
                     $addTransmital = CreateCommunication::where('transac_id', $request->transac_id[$i]);
                     $addTransmital->update($request->except('transac_id'));
                 }
             }
 
             return response()->json(['message' => 'Your entry has been successfully saved.']);
-            
-        }catch(\Throwable $th){
+        } catch (\Throwable $th) {
             return response()->json([
                 'status' => true,
                 'message' => 'Something went wrong!',
