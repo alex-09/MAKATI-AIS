e96999e3f5c5664035c10a25e25fa3da7d9c4eef
unknown <menard.alcantara@simplevia.com>
2023-06-08 15:55:54 +0800

    changes in file name in database

--- app/Repositories/DocumentManagement/Receiving/Communications/ReceiveCommRepository.php
+++ app/Repositories/DocumentManagement/Receiving/Communications/ReceiveCommRepository.php
@@ -26,7 +26,7 @@ class ReceiveCommRepository
 
             ReceiveCommunications::create([
             "transaction_id_num" => $transac_id,
-            "document" => public_path('Document/DocumentManagement/Receiving/Communication/').$docuFile
+            "document" => $docuFile
             ] + $request->validated());
 
             ActionHistory::create([
