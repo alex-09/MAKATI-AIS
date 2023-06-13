4d54f0b9e0882dfc6eba2a01328d2bcd63c72653
unknown <menard.alcantara@simpleviw.com>
2023-06-13 16:26:57 +0800

    finished setup

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -15,19 +15,19 @@ class Cors
      */
     public function handle(Request $request, Closure $next)
     {
-        return $next($request);
-        // ->header('Access-Control-Allow-Origin', 'http://localhost:8000',
-        //     'http://127.0.0.1:80',
-        //     'http://127.0.0.1:80',
-        //     'http://makati-ais.com',
-        //     'http://makati-ais-api.com',
-        //     'http://ais.simplevia.com',
-        //     'http://ais-api.simplevia.com',)
+        return $next($request)
+        ->header('Access-Control-Allow-Origin', 'http://localhost:8000',
+            'http://127.0.0.1:80',
+            'http://127.0.0.1:80',
+            'http://makati-ais.com',
+            'http://makati-ais-api.com',
+            'http://ais.simplevia.com',
+            'http://ais-api.simplevia.com',)
 
-        // ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+        ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
 
-        // ->header('Access-Control-Allow-Headers',' Origin, Content-Type, Accept, Authorization, X-Request-With, Authentication')
+        ->header('Access-Control-Allow-Headers',' Origin, Content-Type, Accept, Authorization, X-Request-With, Authentication')
 
-        // ->header('Access-Control-Allow-Credentials',' true');
+        ->header('Access-Control-Allow-Credentials',' true');
     }
 }
