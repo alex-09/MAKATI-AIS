bd8a13c23a87811bc7c963c9a938982f6bcc4622
unknown <menard.alcantara@simpleviw.com>
2023-06-25 23:13:46 +0800

    allotment dropdown

--- app/Http/Controllers/BAT/ExecutiveBudget/Processor/Allotment/UpdateAllotmentController.php
+++ app/Http/Controllers/BAT/ExecutiveBudget/Processor/Allotment/UpdateAllotmentController.php
@@ -8,7 +8,7 @@ use App\Models\Allotment;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
-use App\Repositories\BAT\Executive\Processor\Allotment\EnrollAllotmentRepository;
+use App\Repositories\BAT\Executive\Processor\Allotment\UpdateAllotmentRepository;
 
 class UpdateAllotmentController extends Controller
 {
